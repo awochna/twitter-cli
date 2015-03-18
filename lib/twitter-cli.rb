4c86a20b141223e9ee1021326425490689b7d3be
Alex Wochna <awochna@email.arizona.edu>
2015-03-18 09:36:18 -0700

    Stub out tweet posting method

--- lib/twitter-cli.rb
+++ lib/twitter-cli.rb
@@ -1,3 +1,11 @@
+require 'thor'
+require 'twitter'
+
 class TwitterCLI < Thor
 
+  desc "post TWEET", "post to your twitter with TWEET"
+  def post(tweet)
+    # tweet posting logic goes here
+    puts "#{Time.now} Posted: #{tweet}"
+  end
 end
